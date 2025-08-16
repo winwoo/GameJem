61ea769839db5e7cb5787c25d55413fa700a2252
하승우 <winwoo4946@gmail.com>
2025-08-16 23:57:51 +0900

    UITitle추가, Dialog추가

--- Assets/01_Scripts/02_UI/UITitle.cs
+++ Assets/01_Scripts/02_UI/UITitle.cs
@@ -1,3 +1,4 @@
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,14 +6,19 @@ public class UITitle : UIBase
 {
     [SerializeField]
     [Link]
-    private Button _btnUpdate;
-    [SerializeField]
-    private DialogData _dialog;
+    private UICDialog _dialog;
 
     public override void OnCreate(object ctx)
     {
         base.OnCreate(ctx);
-        _btnUpdate.onClick.AddListener(OnUpdate);
+    }
+
+    public override async UniTask ShowAsync(object args = null)
+    {
+        await base.ShowAsync(args);
+        await _dialog.StartDialog();
+        await Managers.Scene.LoadSceneAsync(Define.Scene.Game);
+        await CloseUI();
     }
 
     private async void OnUpdate()
