0e87f4b436950f575725225256579d4147bf5693
하승우 <winwoo4946@gmail.com>
2025-08-16 23:27:33 +0900

    다이어로그 추가중

--- Assets/01_Scripts/01_Scene/SceneTitle.cs
+++ Assets/01_Scripts/01_Scene/SceneTitle.cs
@@ -5,8 +5,9 @@ public class SceneTitle : SceneBase
 {
     protected async override UniTask Init()
     {
-        //await Managers.UI.Open<UITitle>();
-        await Managers.UI.Open<UIForlder>();
+        await Managers.UI.Open<UITitle>();
+        //await Managers.UI.Open<UIForlder>();
+        //await Managers.UI.Open<UISystem>();
     }
 
     public override UniTask Dispose()
